1a77974a6f5a7604f66611ff26642da59669b46c
Minoshii <105118168+Minoshii@users.noreply.github.com>
2026-05-14 11:53:00 +0300

    Update easteregg.js

--- public/easteregg.js
+++ public/easteregg.js
@@ -1,29 +1,29 @@
-// --- POLONICA EASTER EGGS ---
-
-// 1. METRO 2033
 let typedMetroKeys = '';
 const secretMetroWord = 'metro';
 const geigerSound = new Audio('geiger.mp3'); 
 geigerSound.volume = 0.5;
 
 document.addEventListener('keydown', (e) => {
-    // Input ve Textarea içindeyken çalışmasını engelle
     if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') return;
 
     typedMetroKeys += e.key.toLowerCase();
-    
     if (typedMetroKeys.length > secretMetroWord.length) {
         typedMetroKeys = typedMetroKeys.slice(1);
     }
 
+    // Şifre kontrolü
     if (typedMetroKeys === secretMetroWord) {
-        geigerSound.play();
-        // Senin kestiğin png dosyasının adı
+        console.log("Aha! Metro Easter Egg tetiklendi!"); // Bu yazı F12 konsolunda çıkacak
+        
+        geigerSound.play().catch(err => console.log("Ses çalma hatası:", err));
+        
+        // Senin yeni dosya adın:
         document.body.style.cursor = "url('cursorkesin.png'), auto"; 
         
         setTimeout(() => {
             document.body.style.cursor = "default";
             typedMetroKeys = ''; 
+            console.log("Radyasyon dağıldı, her şey normale döndü.");
         }, 5000);
     }
 });
